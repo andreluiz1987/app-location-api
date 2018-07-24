e8391bfecf28acd8ca09ae99d66a8a53ff7167ac
André Luiz Gomes Coelho <andre.luiz1987@gmail.com>
2018-07-23 23:20:08 -0300

    update

--- bin/server.js
+++ bin/server.js
@@ -4,7 +4,7 @@ const app = require('../src/app');
 const http = require('http');
 const debug = require('debug')('appLocation:server');
 
-const port = normalizePort(process.env.PORT || '3000');
+const port = process.env.PORT || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
