797f9f1628a2a85dcb48247e1165cd5009dd3d89
andre.coelho <andre.luiz1987@gmail.com>
2018-07-25 14:03:38 -0300

    upadte

--- bin/server.js
+++ bin/server.js
@@ -7,10 +7,7 @@ const debug = require('debug')('appLocation:server');
 const port = process.env.PORT || 3000;
 app.set('port', port);
 
-const server = http.createServer(function(req, res){
-  res.writeHead(200, {"Content-type":"text/plain"});
-  res.end("Ola Mundo!!");
-});
+const server = http.createServer(app);
 server.listen(port);
 
 console.log("API rodando porta " + port);
